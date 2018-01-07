c805e8c89786257cfa68b73f85adc4906bc757ca
AlexStrizh <strizhhh@mail.ru>
2018-01-08 02:02:49 +0300

    Sentence Smash

--- src/kyu8/fundamentals/SentenceSmash.java
+++ src/kyu8/fundamentals/SentenceSmash.java
@@ -2,6 +2,6 @@ package kyu8.fundamentals;
 
 public class SentenceSmash {
     public static String smash(String... words) {
-        return String.join(" ", words).trim();
+        return String.join(" ", words);
     }
 }
