28fc4c1d63f50a88cabf726000f6c7229246295d
AlexStrizh <strizhhh@mail.ru>
2018-01-08 02:33:38 +0300

    Number of People in the Bus, #2

--- src/kyu8/fundamentals/NumberOfPeopleInTheBus.java
+++ src/kyu8/fundamentals/NumberOfPeopleInTheBus.java
@@ -4,12 +4,8 @@ import java.util.ArrayList;
 
 public class NumberOfPeopleInTheBus {
     public static int countPassengers(ArrayList<int[]> stops) {
-        int inBus = 0;
-
-        for (int[] element : stops) {
-            inBus = inBus + element[0] - element[1];
-        }
-
-        return inBus;
+        return stops.stream()
+                .mapToInt(x -> x[0] - x[1])
+                .sum();
     }
 }
