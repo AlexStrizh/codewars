834b9285578d6ab93121d844b1f1d5c3300fcace
AlexStrizh <strizhhh@mail.ru>
2018-01-07 22:26:32 +0300

    Refactor

--- src/kyu8/fundamentals/aneedleinthehaystack/ANeedleInTheHaystack.java
+++ src/kyu8/fundamentals/aneedleinthehaystack/ANeedleInTheHaystack.java
@@ -1,18 +1,12 @@
 package kyu8.fundamentals.aneedleinthehaystack;
 
-public class ANeedleInTheHaystack {
+import java.util.Arrays;
 
+public class ANeedleInTheHaystack {
     private final static String target = "needle";
     private final static String message = "found the needle at position ";
 
     public static String findNeedle(Object[] haystack) {
-
-        int i;
-        for (i = 0; i < haystack.length; i++) {
-            if (haystack[i] == null) continue;
-            if (haystack[i].equals(target)) break;
-        }
-
-        return message + i;
+        return String.format(message + "%d", Arrays.asList(haystack).indexOf(target));
     }
 }
