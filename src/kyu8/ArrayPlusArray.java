1127d4a1e7937e67a4f6406b60d35d37295c9c19
AlexStrizh <strizhhh@mail.ru>
2018-01-09 14:11:28 +0300

    Array plus array, #1

---
+++ src/kyu8/ArrayPlusArray.java
@@ -0,0 +1,16 @@
+/*
+I'm new to coding and now I want to get the sum of two arrays...actually the sum of all their elements.
+I'll appreciate for your help.
+
+P.S. Each array includes only integer numbers. Output is a number too.
+ */
+package kyu8;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class ArrayPlusArray {
+    public static int arrayPlusArray(int[] arr1, int[] arr2) {
+        return Stream.of(arr1, arr2).flatMapToInt(Arrays::stream).sum();
+    }
+}
