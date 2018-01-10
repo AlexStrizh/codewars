3ac9ed00681e2f18db215d1072095e10db7850a8
AlexStrizh <strizhhh@mail.ru>
2018-01-10 22:11:43 +0300

    Remove String Spaces, #1

---
+++ src/kyu8/RemoveStringSpaces.java
@@ -0,0 +1,11 @@
+/*
+Simple, remove the spaces from the string, then return the resultant string.
+ */
+
+package kyu8;
+
+public class RemoveStringSpaces {
+    static String noSpace(final String x) {
+        return x.replace(" ", "");
+    }
+}
