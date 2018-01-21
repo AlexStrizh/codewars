65a85aecb972d2c562f73f6a7bcc39df6f181027
AlexStrizh <strizhhh@mail.ru>
2018-01-21 22:05:31 +0300

    How do I compare numbers, #1

---
+++ src/test/java/kyu8/HowDoICompareNumbersTest.java
@@ -0,0 +1,21 @@
+package kyu8;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HowDoICompareNumbersTest {
+    @Test
+    public void test() throws Exception {
+        for (Object[] t: tests)
+            assertEquals(t[1], HowDoICompareNumbers.whatIs((Integer)t[0]));
+    }
+
+    static final Object[][] tests = {
+            {0, "nothing"},
+            {123, "nothing"},
+            {-1, "nothing"},
+            {42, "everything"},
+            {42 * 42, "everything squared"},
+    };
+}
