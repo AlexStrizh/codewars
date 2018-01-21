ebc786e1cd6f276e80186540965f867e1a311aac
AlexStrizh <strizhhh@mail.ru>
2018-01-22 00:46:52 +0300

    No zeros for heros (условие задачи)

---
+++ src/main/java/kyu8/NoZerosForHeros.java
@@ -0,0 +1,25 @@
+/*
+Numbers ending with zeros are boring.
+
+They might be fun in your world, but not here.
+
+Get rid of them. Only the ending ones.
+
+1450 -> 145
+960000 -> 96
+1050 -> 105
+-1050 -> -105
+
+Zero alone is fine, don't worry about it. Poor guy anyway
+ */
+package kyu8;
+
+import java.util.Arrays;
+
+public class NoZerosForHeros {
+    public static int noBoringZeros(int n) {
+
+return 0;
+    }
+
+}
---
+++ src/test/java/kyu8/NoZerosForHerosTest.java
@@ -0,0 +1,19 @@
+package kyu8;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NoZerosForHerosTest {
+    private static void testing(int actual, int expected) {
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void test1() {
+        System.out.println("Fixed Tests: noBoringZeros");
+        testing(NoZerosForHeros.noBoringZeros(1450), 145);
+        testing(NoZerosForHeros.noBoringZeros(960000), 96);
+        testing(NoZerosForHeros.noBoringZeros(1050), 105);
+        testing(NoZerosForHeros.noBoringZeros(-1050), -105);
+    }
+}
